a536a9e1fe393142902074f72aeea40ac8771d6d
Amr Soliman <thisissoliman@protonmail.com>
2024-07-08 12:56:11 +0300

    opt for next-mdx-remote

--- pages/api/dyn/[slug].ts
+++ pages/api/dyn/[slug].ts
@@ -1,33 +1,36 @@
 import prisma from "@/prisma/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { buildDynamicMDX, buildDynamicMeta } from "nextra/remote";
-
+import { serialize} from 'next-mdx-remote/serialize'
+import {compileMDX} from 'next-mdx-remote/rsc'
+import { MDXRemote } from 'next-mdx-remote'
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const params = req.query
-  let dynamicMdx = {};
-  try {
+ 
+ 
     const r = await prisma.post.findFirst({ where: { slug: params.slug as string } });
-    const t = r != null ? r.content : "";
-    dynamicMdx = await buildDynamicMDX(t, { mdxOptions: {} });
-    return res.json({
-      props: {
-        ...dynamicMdx,
-      },
-    });
-  } catch (error) {
-    console.log(error);
+    console.log(r?.content)
+    const s =await serialize(
+      // Raw MDX contents as a string
+      r!.content,
+      // Optional parameters
+      {
+        // made available to the arguments of any custom MDX component
+        scope: {},
+        // MDX's available options, see the MDX docs for more info.
+        // https://mdxjs.com/packages/mdx/#compilefile-options
+        mdxOptions: {
+          remarkPlugins: [],
+          rehypePlugins: [],
+          format: 'mdx',
+        },
+        // Indicates whether or not to parse the frontmatter from the MDX source
+        parseFrontmatter: true,
+      }
+    )
 
-    dynamicMdx = await buildDynamicMDX(
-      "# Error\n\n## OOPS We Couldn't Get Your Post Please Be patient we Are under Construction\n\n## 🚧",
-      { mdxOptions: {} }
-    );
-    return res.json({
-      props: {
-        error,
-        ...dynamicMdx,
-      },
-    });
-  }
+return res.json(s)
+  
 }
